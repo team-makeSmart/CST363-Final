379e8805344639904960a38a83efaf5ded78a80f
Mcghee-Jake <jmcghee@csumb.edu>
2018-02-08 13:37:46 -0800

    added some animals and exhibits

--- inserts.sql
+++ inserts.sql
@@ -1,33 +1,50 @@
+/*
+
+10 species +
+5 exhibits +
+20 animals -
+8 natural habitats -
+
+	+ = complete
+	- = incomplete
+*/
+
 -- SPECIES ---------------------------------------------------------
 
--- CATS 
 INSERT INTO species VALUES (null, 'Jaguar', 'Panthera onca', 0);
 INSERT INTO species VALUES (null, 'Lion', 'Panthera leo', 0);
 INSERT INTO species VALUES (null, 'Tiger', 'Panthera tigris', 1);
 INSERT INTO species VALUES (null, 'Cheetah', 'Acinonyx jubatus', 0);
-INSERT INTO species VALUES (null, 'Amur Leopard', 'Panthera pardus orientalis', 1);
-INSERT INTO species VALUES (null, 'Ocelot', 'Leopardus pardalis', 0);
-INSERT INTO species VALUES (null, 'Caracal', 'Caracal caracal', 0);
-
--- PRIMATES
 INSERT INTO species VALUES (null, 'Gorilla', 'Gorilla gorilla', 1);
 INSERT INTO species VALUES (null, 'Orangutan', 'Pongo', 1);
 INSERT INTO species VALUES (null, 'Lemur', 'Lemuroidea', 1);
 INSERT INTO species VALUES (null, 'Capuchin', 'Cebinae', 0);
-INSERT INTO species VALUES (null, 'Spider Monkey', 'Ateles', 1);
-INSERT INTO species VALUES (null, 'Squirrel Monkey', 'Saimiri', 0);
-INSERT INTO species VALUES (null, 'Pygmy Marmoset', 'Cebuella pygmaea', 0);
-
--- BIRDS
-INSERT INTO species VALUES (null, 'Bald Eagle', 'Haliaeetus leucocephalus', 0);
-INSERT INTO species VALUES (null, 'Red-tailed Hawk', 'Buteo jamaicensis', 0);
-
--- OTHER
 INSERT INTO species VALUES (null, 'Rhinoceros', 'Rhinocerotidae', 0);
 INSERT INTO species VALUES (null, 'African Elephant', 'Loxodonta africana', 0);
 
 
 -- EXHIBIT ---------------------------------------------------------
 
-INSERT INTO species VALUES (null, 'Cat Canyon');
-INSERT INTO species VALUES (null, 'Primate Paridise');   
+
+INSERT INTO exhibit VALUES (null, 'Forbidden Forest');
+INSERT INTO exhibit VALUES (null, 'Joyous Jungle'); 
+INSERT INTO exhibit VALUES (null, 'Cataclysmic Canyon');   
+INSERT INTO exhibit VALUES (null, 'Panoramic Plains');
+INSERT INTO exhibit VALUES (null, 'Wonderous Wetlands');
+
+-- ANIMALS ---------------------------------------------------------
+
+INSERT INTO animal VALUES (null, 'Aurora', '2016-10-16', 'F', 1, 1);
+INSERT INTO animal VALUES (null, 'Aemilia', '2008-08-23', 'F', 1, 1);
+INSERT INTO animal VALUES (null, 'Nora', '2013-08-11', 'F', 3, 2);
+INSERT INTO animal VALUES (null, 'Orion', '2012-12-23', 'M', 3, 2);
+INSERT INTO animal VALUES (null, 'Ivona', '2009-01-24', 'F', 5, 3);
+INSERT INTO animal VALUES (null, 'Sankar', '2009-12-03', 'M', 5, 3);
+INSERT INTO animal VALUES (null, 'Amala', '2007-03-13', 'F', 4, 4);
+INSERT INTO animal VALUES (null, 'Raffael', '2006-06-16', 'M', 4, 4);
+INSERT INTO animal VALUES (null, 'Marianne', '1997-01-05', 'F', 2, 5);
+INSERT INTO animal VALUES (null, 'Mahir', '1999-12-05', 'M', 2, 5);
+
+
+
+
